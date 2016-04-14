253b1fdba017d7ecf75be0ef3a7c299a4c7ba785
amir <amirazad100@gmail.com>
2016-04-14 22:07:08 +0430

    adding database initialize

---
+++ app/src/main/java/com/example/amir/koolak/Items.java
@@ -0,0 +1,122 @@
+package com.example.amir.koolak;
+
+import java.util.ArrayList;
+
+/**
+ * Created by amir on 4/14/2016.
+ */
+public class Items {
+
+    private String pName;
+    private int id;
+    public boolean clicked;
+    private ArrayList<SubCategory> mSubCategoryList;
+
+    public Items(String pName, int id, ArrayList<SubCategory> mSubCategoryList) {
+        super();
+        this.pName = pName;
+        this.id = id;
+        this.clicked = false;
+        this.mSubCategoryList = mSubCategoryList;
+    }
+
+    public boolean hasChild() {
+        return mSubCategoryList.size() > 0;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public ArrayList<SubCategory> getmSubCategoryList() {
+        return mSubCategoryList;
+    }
+
+    public void setmSubCategoryList(ArrayList<SubCategory> mSubCategoryList) {
+        this.mSubCategoryList = mSubCategoryList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * second level item
+     */
+
+    public static class SubCategory {
+
+        private String pSubCatName;
+        private int id;
+        public boolean clicked;
+        private ArrayList<ItemList> mItemListArray;
+
+        public SubCategory(String pSubCatName, int id,
+                           ArrayList<ItemList> mItemListArray) {
+            super();
+            this.pSubCatName = pSubCatName;
+            this.id = id;
+            this.clicked = false;
+            this.mItemListArray = mItemListArray;
+        }
+
+        public boolean hasChild() {
+            return mItemListArray.size() > 0;
+        }
+
+        public String getpSubCatName() {
+            return pSubCatName;
+        }
+
+        public void setpSubCatName(String pSubCatName) {
+            this.pSubCatName = pSubCatName;
+        }
+
+        public ArrayList<ItemList> getmItemListArray() {
+            return mItemListArray;
+        }
+
+        public void setmItemListArray(ArrayList<ItemList> mItemListArray) {
+            this.mItemListArray = mItemListArray;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * third level item
+         */
+        public static class ItemList {
+
+            private String itemName;
+            private int id;
+
+            public ItemList(String itemName, int id) {
+                super();
+                this.itemName = itemName;
+                this.id = id;
+            }
+
+            public String getItemName() {
+                return itemName;
+            }
+
+            public void setItemName(String itemName) {
+                this.itemName = itemName;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+        }
+
+    }
+
+}
+
--- app/src/main/java/com/example/amir/koolak/MainActivity.java
+++ app/src/main/java/com/example/amir/koolak/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.amir.koolak;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
@@ -12,27 +21,68 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        SQLiteDatabase db = openOrCreateDatabase("koolak5", MODE_PRIVATE, null);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='content'", null);
+        if (c.getCount() == 0) {
+            db.execSQL("CREATE TABLE content (id int(3), Pid int(3), type int(1), title varchar(50));");
+            db.execSQL("CREATE TABLE diagram (id int(3), title text, color varchar(6), arms int(1), arm1 text, arm2 text, arm3 text, arm4 text, arm5 text, arm6 text," +
+                    " et1_1 text, et1_2 text,et1_3 text, et1_4 text, et1_5 text, et2_1 text, et2_2 text,et2_3 text, et2_4, et2_5, et3_1 text, et3_2 text,et3_3 text, et3_4 text, et3_5 text," +
+                    " et4_1 text, et4_2 text,et4_3 text, et4_4 text, et4_5 text, et5_1 text, et5_2 text,et5_3 text, et5_4, et5_5, et6_1 text, et6_2 text,et6_3 text, et6_4 text, et6_5 text," +
+                    "sentence text);");
+            db.execSQL("CREATE TABLE story (id int(3), color varchar(6), title text, content text, writer text);");
+            db.execSQL("CREATE TABLE endday (id int(3), color varchar(6), content text);");
+            db.execSQL("CREATE TABLE endweek (id int(3), color varchar(6), best_sentence text, time1 text, time2 text, time3 text);");
+
+            Resources res = getResources();
+            XmlResourceParser content = res.getXml(R.xml.content);
+            setResources(db, content, "content");
+            XmlResourceParser diagram = res.getXml(R.xml.diagram);
+            setResources(db, diagram, "diagram");
+            XmlResourceParser story = res.getXml(R.xml.story);
+            setResources(db, story, "story");
+            XmlResourceParser endDay = res.getXml(R.xml.end_day);
+            setResources(db, endDay, "endday");
+            XmlResourceParser endWeek = res.getXml(R.xml.end_week);
+            setResources(db, endWeek, "endweek");
         }
+        c.close();
 
-        return super.onOptionsItemSelected(item);
     }
+
+    private void setResources(SQLiteDatabase db, XmlPullParser content, String tableName) {
+        ContentValues row = new ContentValues();
+        String name, value;
+        try {
+            int eventType = content.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG && content.getName().equals("row")) {
+                    row.clear();
+                    eventType = content.next();
+                    while (!(eventType == XmlPullParser.END_TAG && content.getName().equals("row"))) {
+                        if (eventType == XmlPullParser.START_TAG) {
+                            name = content.getName();
+                            eventType = content.next();
+                            if (eventType == XmlPullParser.TEXT) {
+                                value = content.getText();
+                                row.put(name, value);
+                            }
+                        }
+                        eventType = content.next();
+                    }
+                    db.insert(tableName, null, row);
+                }
+                eventType = content.next();
+            }
+        } catch (XmlPullParserException | IOException e) {
+            Log.d("LEE", e.toString());
+        }
+    }
+
 }
+
+
